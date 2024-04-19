f1c8a7ceaf85f99aa5a6ada18b44b84f88f2ab6c
Guilherme <g.satilio@gmail.com>
2024-04-19 17:02:17 -0300

    Add project files.

---
+++ Trab-NroPrimo/Program.cs
@@ -0,0 +1,35 @@
+﻿//1 - faça um programa que leia um número (int, float, etc) e informe se esse número é primo (por ele msm ou 1)//
+
+float numero, numeroantes, resto;
+int contador;
+
+Console.WriteLine("Por favor, informe um número:");
+numero = float.Parse(Console.ReadLine());
+while(numero <= 0)
+{
+    Console.WriteLine("Por favor, informe um número MAIOR QUE 1:");
+    numero = float.Parse(Console.ReadLine());
+}
+numeroantes = 0;
+resto = 0;
+contador = 0;
+
+while ((numeroantes <= numero))
+{
+    resto = (numero % numeroantes);
+    if(resto == 0)
+    {
+        contador++;
+    }
+
+    numeroantes++;
+}
+
+if (contador == 2){
+    Console.WriteLine("O número " + numero + " é um número primo.");
+} else
+{
+    Console.WriteLine("O número " + numero + " NÃO é um número primo.");
+}
+
+Console.ReadKey();
