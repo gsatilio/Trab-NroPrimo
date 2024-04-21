67bdd9a32ff55a51a25f5683fd0c5e27c1932c7a
Guilherme <g.satilio@gmail.com>
2024-04-21 18:47:05 -0300

    melhoria 1

--- Trab-NroPrimo/Program.cs
+++ Trab-NroPrimo/Program.cs
@@ -1,35 +1,46 @@
 ﻿//1 - faça um programa que leia um número (int, float, etc) e informe se esse número é primo (por ele msm ou 1)//
 
 float numero, numeroantes, resto;
+string sair;
 int contador;
+sair = "N";
 
-Console.WriteLine("Por favor, informe um número:");
-numero = float.Parse(Console.ReadLine());
-while(numero <= 0)
+while (sair != "S")
 {
-    Console.WriteLine("Por favor, informe um número MAIOR QUE 1:");
+    Console.WriteLine("-----------------------------------------------------------");
+    Console.WriteLine("Por favor, informe um número para saber se ele é do tipo primo ou não:");
     numero = float.Parse(Console.ReadLine());
-}
-numeroantes = 0;
-resto = 0;
-contador = 0;
-
-while ((numeroantes <= numero))
-{
-    resto = (numero % numeroantes);
-    if(resto == 0)
+    while (numero <= 0)
     {
-        contador++;
+        Console.WriteLine("Por favor, informe um número que seja MAIOR QUE 1:");
+        numero = float.Parse(Console.ReadLine());
+    }
+    numeroantes = 0;
+    resto = 0;
+    contador = 0;
+
+    while ((numeroantes <= numero))
+    {
+        resto = (numero % numeroantes);
+        if (resto == 0)
+        {
+            contador++;
+        }
+
+        numeroantes++;
     }
 
-    numeroantes++;
-}
+    if (contador == 2)
+    {
+        Console.WriteLine("O número " + numero + " é um número primo.");
+    }
+    else
+    {
+        Console.WriteLine("O número " + numero + " NÃO é um número primo.");
+    }
 
-if (contador == 2){
-    Console.WriteLine("O número " + numero + " é um número primo.");
-} else
-{
-    Console.WriteLine("O número " + numero + " NÃO é um número primo.");
+    Console.WriteLine("Digite S para sair do programa ou aperte qualquer tecla para repetir.");
+    sair = Console.ReadLine();
 }
 
 Console.ReadKey();
